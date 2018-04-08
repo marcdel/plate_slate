a51247d52bbbc2f219daf6fb90909af051343646
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-04-08 10:03:06 -0400

    Move menu related types and queries to a separate module

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,78 +1,9 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
-  alias PlateSlateWeb.Resolvers
+
+  import_types(__MODULE__.MenuTypes)
 
   query do
-    @desc "The list of available items on the menu"
-    field(:menu_items, list_of(:menu_item)) do
-      arg(:filter, :menu_item_filter)
-
-      @desc "Sorts items in asc of desc order by item name"
-      arg(:order, type: :sort_order, default_value: :asc)
-
-      resolve(&Resolvers.Menu.menu_items/3)
-    end
-  end
-
-  scalar :date do
-    parse(fn input ->
-      with %Absinthe.Blueprint.Input.String{value: value} <- input,
-           {:ok, date} <- Date.from_iso8601(value) do
-        {:ok, date}
-      else
-        _ -> :error
-      end
-    end)
-
-    serialize(fn date ->
-      Date.to_iso8601(date)
-    end)
-  end
-
-  enum :sort_order do
-    value(:asc)
-    value(:desc)
-  end
-
-  @desc "Filtering options for the list of menu items"
-  input_object :menu_item_filter do
-    @desc "Matching a name"
-    field(:name, :string)
-
-    @desc "Matching a category name"
-    field(:category, :string)
-
-    @desc "Matching a tag"
-    field(:tag, :string)
-
-    @desc "Priced above a value"
-    field(:priced_above, :float)
-
-    @desc "Priced below a value"
-    field(:priced_below, :float)
-
-    @desc "Added before a date"
-    field(:added_before, :date)
-
-    @desc "Added after a date"
-    field(:added_after, :date)
-  end
-
-  @desc "An item on the menu"
-  object :menu_item do
-    @desc "Primary key of the Menu.Items table"
-    field(:id, :id)
-
-    @desc "The name of the menu item"
-    field(:name, :string)
-
-    @desc "A description of the item"
-    field(:description, :string)
-
-    @desc "The date the item was added on"
-    field(:added_on, :date)
-
-    @desc "The decimal price of the item (ex: 4.5)"
-    field(:price, :float)
+    import_fields(:menu_queries)
   end
 end
---
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -0,0 +1,65 @@
+defmodule PlateSlateWeb.Schema.MenuTypes do
+  use Absinthe.Schema.Notation
+  alias PlateSlateWeb.Resolvers
+
+  object :menu_queries do
+    @desc "The list of available items on the menu"
+    field(:menu_items, list_of(:menu_item)) do
+      arg(:filter, :menu_item_filter)
+      arg(:order, type: :sort_order, default_value: :asc)
+      resolve(&Resolvers.Menu.menu_items/3)
+    end
+  end
+
+  object :menu_item do
+    field(:id, :id)
+    field(:name, :string)
+    field(:description, :string)
+    field(:added_on, :date)
+    field(:price, :float)
+  end
+
+  @desc "Filtering options for the menu item list"
+  input_object :menu_item_filter do
+    @desc "Matching a name"
+    field(:name, :string)
+
+    @desc "Matching a category name"
+    field(:category, :string)
+
+    @desc "Matching a tag"
+    field(:tag, :string)
+
+    @desc "Priced above a value"
+    field(:priced_above, :float)
+
+    @desc "Priced below a value"
+    field(:priced_below, :float)
+
+    @desc "Added to the menu before this date"
+    field(:added_before, :date)
+
+    @desc "Added to the menu after this date"
+    field(:added_after, :date)
+  end
+
+  enum :sort_order do
+    value(:asc)
+    value(:desc)
+  end
+
+  scalar :date do
+    parse(fn input ->
+      with %Absinthe.Blueprint.Input.String{value: value} <- input,
+           {:ok, date} <- Date.from_iso8601(value) do
+        {:ok, date}
+      else
+        _ -> :error
+      end
+    end)
+
+    serialize(fn date ->
+      Date.to_iso8601(date)
+    end)
+  end
+end
